e3298aee447f4f913ff2417d121876a2cdf8eb0a
Pei Hsuan Tsai <c20712983@gmail.com>
2018-04-24 21:22:24 +0800

    Move function.

--- user.go
+++ user.go
@@ -1,16 +1,83 @@
 package acl
 
 import (
+	"fmt"
 	"net/http"
 
 	"bitbucket.org/linkernetworks/aurora/src/entity"
 	"bitbucket.org/linkernetworks/aurora/src/service/mongo"
+	"bitbucket.org/linkernetworks/aurora/src/service/session"
 	restful "github.com/emicklei/go-restful"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
-type Callback func(*mongo.Session, *http.Request) (*entity.User, error)
+const SessionKey = "ses"
 
-func GetCurrentUserRestful(getCurrentUser Callback, ses *mongo.Session, req *restful.Request) (*entity.User, error) {
+func GetCurrentUserRestful(ses *mongo.Session, req *restful.Request) (*entity.User, error) {
+	return GetCurrentUser(ses, req.Request)
+}
 
-	return getCurrentUser(ses, req.Request)
+// GetCurrentUser get current user data with login session and return user data
+// excluding sensitive data like password.
+func GetCurrentUser(ses *mongo.Session, req *http.Request) (*entity.User, error) {
+	email, err := GetCurrentUserEmail(req)
+	if err != nil {
+		return nil, err
+	}
+
+	user := entity.User{}
+	q := bson.M{"email": email}
+	projection := bson.M{"password": 0}
+	if err := ses.C(entity.UserCollectionName).Find(q).Select(projection).One(&user); err != nil {
+		if err == mgo.ErrNotFound {
+			return nil, fmt.Errorf("user document not found.")
+		}
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+// GetCurrentUserWithPassword get current user data with login session and return all user data
+// including sensitive data like encrypted password.
+func GetCurrentUserWithPassword(ses *mongo.Session, req *http.Request) (*entity.User, error) {
+	email, err := GetCurrentUserEmail(req)
+	if err != nil {
+		return nil, err
+	}
+
+	user := entity.User{}
+	q := bson.M{"email": email}
+	if err := ses.C(entity.UserCollectionName).Find(q).One(&user); err != nil {
+		if err == mgo.ErrNotFound {
+			return nil, fmt.Errorf("user document not found.")
+		}
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func GetCurrentUserEmail(req *http.Request) (string, error) {
+	token := req.Header.Get("Authorization")
+	if len(token) == 0 {
+		return "", fmt.Errorf("Authorization token is missing.")
+	}
+
+	session, err := session.Service.Store.Get(req, SessionKey)
+	if err != nil {
+		return "", err
+	}
+
+	val, found := session.Values["email"]
+	if !found {
+		return "", fmt.Errorf("session email is not set.")
+	}
+
+	email, ok := val.(string)
+	if !ok {
+		return "", fmt.Errorf("session email value type is invalid.")
+	}
+	return email, err
 }
